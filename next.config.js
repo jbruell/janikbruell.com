c3250fcde6a076e8ff8389ba6d6015efe165270c
Janik Brüll <github@janikbruell.com>
2022-03-23 22:03:44 +0100

    Include styles/ directory in SASS compiler path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 // @ts-check
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.resolve(__dirname, "./src/styles")],
+  },
 };
 
 module.exports = nextConfig;
