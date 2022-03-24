c36b78713eaa43f4a029fd6bbd3ebd9967946a80
Janik Brüll <github@janikbruell.com>
2022-03-24 18:37:07 +0100

    Setup typography variations and rules

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document(): JSX.Element {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
